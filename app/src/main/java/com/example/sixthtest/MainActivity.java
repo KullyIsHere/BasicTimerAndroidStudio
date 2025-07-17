a32e6e59c363ecbfc775401db66a034ed8b0a48e
KullyIsHere <abdulmaliq.adegbite@gmail.com>
2025-07-17 19:01:49 +0100

    Added Notifications

--- app/src/main/java/com/example/sixthtest/MainActivity.java
+++ app/src/main/java/com/example/sixthtest/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.sixthtest;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.Manifest;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    //confirms notifications
+    private final ActivityResultLauncher<String> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+        @Override
+        public void onActivityResult(Boolean o) {
+            if (o) {
+                Toast.makeText(MainActivity.this, "Post notification permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Post notification permission not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            activityResultLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+        }
+
     }
 
     public void MoveTimer(View view) {
--- app/src/main/java/com/example/sixthtest/Timer.java
+++ app/src/main/java/com/example/sixthtest/Timer.java
@@ -1,13 +1,24 @@
 package com.example.sixthtest;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.TextView;
+import android.Manifest;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,6 +27,17 @@ import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class Timer extends AppCompatActivity {
+    // confirms notifications
+    private final ActivityResultLauncher<String> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+        @Override
+        public void onActivityResult(Boolean o) {
+            if (o) {
+                Toast.makeText(Timer.this, "Post notification permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(Timer.this, "Post notification permission not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +59,19 @@ public class Timer extends AppCompatActivity {
 
         // Timer script
         TextView textView = findViewById(R.id.textView);
-        long timerDuration = TimeUnit.MINUTES.toMillis(timerValue);
+        long timerDuration = TimeUnit.MINUTES.toMillis(1);
         long ticksInterval = 10;
+
+        //Notification setup
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(Timer.this, "test")
+                .setSmallIcon(R.drawable.baseline_circle_notifications_24)
+                .setContentTitle("Study pet")
+                .setContentText("Timer Finished")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
         //Countdown timer
         new CountDownTimer(timerDuration, ticksInterval) {
             long millis = 1000;
@@ -60,9 +93,21 @@ public class Timer extends AppCompatActivity {
             @Override
             public void onFinish() {
                 textView.setText("Finished");
+                //Here's where the notification is sent.
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && ActivityCompat.checkSelfPermission(Timer.this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    activityResultLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+                } else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        NotificationChannel notificationChannel = new NotificationChannel("test",getString(R.string.app_name), NotificationManager.IMPORTANCE_DEFAULT);
+                        notificationChannel.setDescription("Timer Finished");
+                        notificationManager.createNotificationChannel(notificationChannel);
+                        notificationManager.notify(10,builder.build());
+                    }
+                }
 
             }
         }.start();
+
     }
 
     public void ReturnSelect(View view) {
